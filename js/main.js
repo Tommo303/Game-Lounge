dae3404262fc53d9528a7f5be79bbeb984868557
Tommo303 <63849418+Tommo303@users.noreply.github.com>
2021-07-30 20:13:46 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+M.AutoInit();
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
